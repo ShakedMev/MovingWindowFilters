68edebcf52303d48a8ebfc583f1a5010a886a581
Shaked <96763878+ShakedMev@users.noreply.github.com>
2023-06-29 17:30:45 +0300

    Added comments

--- src/main/kotlin/filters/movingwindowfilters/MovingWindowFilter.kt
+++ src/main/kotlin/filters/movingwindowfilters/MovingWindowFilter.kt
@@ -7,24 +7,36 @@ import java.util.LinkedList
  */
 sealed class MovingWindowFilter {
     /**
-     * The number of samples to be included in the calculation. If not enough samples were
-     * provided yet, then the calculation will simply use less samples.
+     * The number of samples to be included in the calculation.
      */
     abstract var window: Int
 
     /**
-     * The calculation to use on the samples list in [calculate]
+     * The calculation to use on the samples list in [calculate].
      */
     protected abstract val calculation: (LinkedList<Double>) -> Double
 
     private val latestSamples = LinkedList<Double>()
 
+    /**
+     * @param newSample - The newest sample to include in the calculation.
+     * @return The calculation result, AKA the latest output of the filter.
+     *         If less samples than the window size were provided, then the
+     *         calculation will simply use less samples.
+     */
     fun calculate(newSample: Double): Double {
         while(latestSamples.size >= window) latestSamples.removeLast()
         latestSamples.addFirst(newSample)
         return calculation.invoke(latestSamples)
     }
 
+    /**
+     * Clears all the previous samples and attempts to fill the samples list with new ones.
+     * If less samples than the window size are provided, then the next calculation will
+     * simply use less samples. If more samples than the window size are provided, then
+     * only the first ones will be included.
+     * > To clear all the previous samples without providing new ones, use [clear].
+     */
     fun reset(newValues: DoubleArray) {
         latestSamples.clear()
         for(i in 0..newValues.size) {
@@ -32,15 +44,29 @@ sealed class MovingWindowFilter {
         }
     }
 
+    /**
+     * Clears all the previous samples and attempts to fill the samples list with new ones.
+     * If less samples than the window size are provided, then the next calculation will
+     * simply use less samples. If more samples than the window size are provided, then
+     * only the first ones will be included.
+     * > To clear all the previous samples without providing new ones, use [clear].
+     */
     fun reset(newValues: Collection<Double>) {
         reset(newValues.toDoubleArray())
     }
 
+    /**
+     * Clears all the previous samples, and fills the samples list with the new value.
+     * To clear all the previous samples without providing new ones, use [clear].
+     */
     fun reset(newValue: Double) {
         reset(DoubleArray(window) {newValue})
     }
 
-    fun empty() {
+    /**
+     * Clears all the previous samples.
+     */
+    fun clear() {
         latestSamples.clear()
     }
 }
