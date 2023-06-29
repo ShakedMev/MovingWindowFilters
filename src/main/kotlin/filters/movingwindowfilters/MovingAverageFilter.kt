2406813fe3b8bbc303e81e096e96b55036bf7a93
שקד <96763878+ShakedMev@users.noreply.github.com>
2023-06-29 18:03:41 +0300

    Fix typo in MovingAverageFilter

--- src/main/kotlin/filters/movingwindowfilters/MovingAverageFilter.kt
+++ src/main/kotlin/filters/movingwindowfilters/MovingAverageFilter.kt
@@ -7,7 +7,7 @@ import java.util.LinkedList
  * ```
  * Since filters have memory, use a separate instance for every input stream.
  * ```
- * @param window - The number of samples to be included in the median calculation.
+ * @param window - The number of samples to be included in the average calculation.
  *                 Assuming calculate is called periodically, the period times the
  *                 window is the time frame of the filter. The filter will more-or-less
  *                 cancel out dynamics that happen in a shorter time frame than this,
